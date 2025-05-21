b716ecee743bad17277bfc850309c157cf992080
Sipho Nkebe <siphonkebe@gmail.com>
2025-05-21 15:50:45 +0200

    fix the video play issue

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -2,7 +2,7 @@ import type { videoTypes } from "../types"
 
 function VideoPlayer({ videoUrl, controls, loop, autoPlay }: videoTypes) {
   return (
-    <video autoPlay={autoPlay} loop={loop} controls={controls} className="w-full">
+    <video key={videoUrl} autoPlay={autoPlay} loop={loop} controls={controls} muted={true} className="w-full">
         <source src={videoUrl} type="video/mp4"/>
       </video>
   )
