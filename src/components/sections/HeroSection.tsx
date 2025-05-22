f63fe932d1b23598d6c56690b903222495f6bbc5
Sipho Nkebe <siphonkebe@gmail.com>
2025-05-22 13:03:13 +0200

    ship it

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,9 +1,28 @@
 import { useEffect } from "react";
+import { motion } from "framer-motion";
 
 import Button from "../ui/Button";
 import VideoPlayer from "../VideoPlayer";
 import { useWindowSize } from "usehooks-ts";
 import { useVideo } from "../../contexts/HeroVideoContext";
+
+const containerVariants = {
+  hidden: { opacity: 0, y: 0 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.35,
+      when: "beforeChildren",
+      staggerChildren: 0.15,
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
 
 function HeroSection() {
   const { width } = useWindowSize();
@@ -20,32 +39,45 @@ function HeroSection() {
           <VideoPlayer
             videoUrl="/videos/mobile/UA_Theme_1_MB.mp4"
             controls={false}
-            loop={true}
-            autoPlay={true}
-            muted={true}
+            loop
+            autoPlay
+            muted
           />
         ) : (
           <VideoPlayer
             videoUrl="/videos/desktop/UA_Theme_1_DT.mp4"
             controls={false}
-            loop={true}
-            autoPlay={true}
-            muted={true}
+            loop
+            autoPlay
+            muted
           />
         )}
 
         {/* Desktop */}
-        <div className="hidden md:block absolute top-1/2 left-1/2 md:translate-x-3/12 lg:translate-x-6/12 -translate-y-1/2 space-y-8">
-          <h1 className="md:text-5xl md:leading-10 lg:leading-18 lg:text-[86px] font-plak-condensed">
+        <motion.div
+          className="hidden md:block absolute top-1/2 left-1/2 md:translate-x-3/12 lg:translate-x-6/12 -translate-y-1/2 space-y-8"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h1
+            variants={childVariants}
+            className="md:text-5xl md:leading-10 lg:leading-18 lg:text-[86px] font-plak-condensed"
+          >
             CHECK <br /> OUT OUR <br /> LATEST <br /> GEAR
-          </h1>
-          <p className="md:text-[11px] lg:text-[12px] font-plak-regular">
+          </motion.h1>
+          <motion.p
+            variants={childVariants}
+            className="md:text-[11px] lg:text-[12px] font-plak-regular"
+          >
             Advanced tech, superior comfort and all <br /> performance is what
             our latest UA gear is all <br /> about. Built to go further, push
             hard and break <br /> your boundaries.
-          </p>
-          <Button>Shop UA New Arrivals</Button>
-        </div>
+          </motion.p>
+          <motion.div variants={childVariants}>
+            <Button>Shop UA New Arrivals</Button>
+          </motion.div>
+        </motion.div>
 
         {/* Mobile */}
         <div className="space-y-6 md:hidden px-4 pt-8">
