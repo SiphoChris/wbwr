8b8c15c4cec2c5834dbfa46cd6024b4999593858
Sipho Nkebe <siphonkebe@gmail.com>
2025-05-22 10:30:14 +0200

    ship it

--- src/components/Preloader.tsx
+++ src/components/Preloader.tsx
@@ -3,7 +3,7 @@ function Preloader() {
     <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 z-50 flex items-center justify-center">
       <div className="text-center">
         <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white mx-auto mb-4"></div>
-        {/* <p className="text-white text-xl font-plak-regular">Loading...</p> */}
+        <p className="text-white text-xl font-plak-condensed">LOADING...</p>
       </div>
     </div>
   );
